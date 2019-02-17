01da23a03f67cf6f03d5dd4ce9e16c83d094739e
Vedran Pavic <vedran.pavic@gmail.com>
2019-02-17 20:56:04 +0100

    Upgrade dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,10 @@
+import org.springframework.boot.gradle.plugin.SpringBootPlugin
+
 plugins {
     java
     checkstyle
     id("com.github.ben-manes.versions") version "0.20.0"
-    id("org.springframework.boot") version "2.1.2.RELEASE"
+    id("org.springframework.boot") version "2.1.3.RELEASE"
 }
 
 java {
@@ -14,17 +16,17 @@ repositories {
 }
 
 dependencies {
-    implementation(platform("org.springframework.boot:spring-boot-dependencies:2.1.2.RELEASE"))
+    implementation(platform(SpringBootPlugin.BOM_COORDINATES))
 
     implementation("org.springframework.boot:spring-boot-starter-web")
 
     implementation("com.github.ben-manes.caffeine:caffeine")
-    implementation("com.nimbusds:nimbus-jose-jwt:6.7")
+    implementation("com.nimbusds:nimbus-jose-jwt:7.0")
     implementation("com.nimbusds:oauth2-oidc-sdk:6.5")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 checkstyle {
-    toolVersion = "8.16"
+    toolVersion = "8.17"
 }
