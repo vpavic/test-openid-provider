284120c5f7db881a934e0a80d63528095aa85cbe
Vedran Pavic <vedran.pavic@gmail.com>
2019-06-27 19:55:11 +0200

    Upgrade dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,9 +2,9 @@ import org.springframework.boot.gradle.plugin.SpringBootPlugin
 
 plugins {
     java
-    id("com.diffplug.gradle.spotless") version "3.23.0"
+    id("com.diffplug.gradle.spotless") version "3.23.1"
     id("com.github.ben-manes.versions") version "0.21.0"
-    id("org.springframework.boot") version "2.1.5.RELEASE"
+    id("org.springframework.boot") version "2.1.6.RELEASE"
 }
 
 java {
@@ -21,7 +21,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
 
     implementation("com.github.ben-manes.caffeine:caffeine")
-    implementation("com.nimbusds:nimbus-jose-jwt:7.2.1")
+    implementation("com.nimbusds:nimbus-jose-jwt:7.3")
     implementation("com.nimbusds:oauth2-oidc-sdk:6.13")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
